59b992a5006b7a2c76b5ff20f4b695f4946857cc
fortunto2 <info@3dstr.ru>
2016-07-19 18:17:44 +0500

    fix

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ meteor-videojs
 v5.10.4
 A meteor package for videoJS
 
-    meteor add jrudio:videojs
+    meteor add lifefilm:videojs
 
 This package only supports the default videojs language
--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'jrudio:videojs',
+  name: 'lifefilm:videojs',
   summary: 'VideoJS v5.10.4',
   version: '5.10.4',
-  git: 'https://github.com/jrudio/meteor-videojs.git'
+  git: 'https://github.com/fortunto2/meteor-videojs.git'
 })
 
 Package.onUse(function(api) {
